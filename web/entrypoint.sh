47ad1e459629b718c5914420ba8d471d8a72a775
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2021-03-28 23:30:26 +0500

    refactoring

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python manage.py flush --no-input
-python manage.py migrate
-python3 manage.py collectstatic
+python manage.py migrate --no-input
+python3 manage.py collectstatic --no-input
 
 exec "$@"
