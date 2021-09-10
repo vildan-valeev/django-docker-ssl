f858909d136c929b790d16a9c2d163ec56347822
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2021-09-10 14:09:38 +0500

    deinsteindesign.net config

--- web/upload/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from upload.models import Person, City, Event
-
-admin.site.register(City)
-admin.site.register(Person)
-admin.site.register(Event)
